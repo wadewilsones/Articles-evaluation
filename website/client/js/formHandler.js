3b089f41f7d84e7ef2074b4c63528fea140b722c
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-04-16 12:11:42 -0700

    Update formHandler.js
    Add if/else statement for URL validation

--- website/client/js/formHandler.js
+++ website/client/js/formHandler.js
@@ -1,17 +1,17 @@
 function handleSubmit (event) {
     let formUrl = document.getElementById('form-URL').value;
+         if(checkForName(formUrl)){
+            let formUrl = document.getElementById('form-URL').value;
     event.preventDefault ()
-    fetch (`http://localhost:8082/save`, 
+    fetch (`http://localhost:8082/analyze`, 
     {method:'POST',
     credentials:'same-origin',
     mode:'cors',
     headers:{'Content-Type':'application/json'},
     body:JSON.stringify({formUrl:formUrl}),   
     })
-
+    .then (console.log (newData)//(res => res.json()) 
+    )}
+   else {console.log ('Invalid URL')}
 }
-
-
-
-
-
+let results = document.getElementById ('results');
