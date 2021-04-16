d1ff371e439bf754098a1eae6c31ad84f173f2a2
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-04-16 14:02:30 -0700

    Update formHandler.js

--- website/client/js/formHandler.js
+++ website/client/js/formHandler.js
@@ -1,17 +1,36 @@
+//Running onclick event
+
 function handleSubmit (event) {
-    let formUrl = document.getElementById('form-URL').value;
-         if(checkForName(formUrl)){
-            let formUrl = document.getElementById('form-URL').value;
-    event.preventDefault ()
-    fetch (`http://localhost:8082/analyze`, 
-    {method:'POST',
-    credentials:'same-origin',
-    mode:'cors',
-    headers:{'Content-Type':'application/json'},
-    body:JSON.stringify({formUrl:formUrl}),   
-    })
-    .then (console.log (newData)//(res => res.json()) 
-    )}
+   event.preventDefault();
+   let formUrl = document.getElementById('form-URL').value;
+   if(checkForName(formUrl)){
+      async function addData () {
+         
+         let answer = await fetch (`http://localhost:8082/analyze`, 
+         {method:'POST',
+          credentials:'same-origin',
+          mode:'cors',
+          headers:{'Content-Type':'application/json'},
+          body:JSON.stringify({formUrl}),
+       })
+         .then ((res) => res.json())
+         
+         .then ((res)=> {displayResponse(res), console.log (res)})
+         //let content = await answer.text();
+   }
+
+   addData();}
+
    else {console.log ('Invalid URL')}
 }
-let results = document.getElementById ('results');
+
+//Display response function
+
+function displayResponse (res) {
+
+   
+   document.getElementById('confidence').innerHTML = "Confindens: " + res.confidence + "%";
+   document.getElementById('Irony').innerHTML = "Irony:  " + res.irony.toLowerCase();
+   document.getElementById('Score').innerHTML = "Score tag: " + res.score_tag.toLowerCase();
+   document.getElementById('Subjectivity').innerHTML = "Subjectivity: " + res.subjectivity.toLowerCase();
+}
