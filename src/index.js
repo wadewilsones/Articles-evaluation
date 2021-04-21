aa20ab8025078892a71fed7270a677a56b4ded68
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-04-21 16:07:39 -0700

    Create index.js
    Creation of index.js. Include imports from "./js/formHandler" and "./js/nameChecker", and scss-files.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { handleSubmit } from "./js/formHandler"
+import { displayResponse } from "./js/formHandler"
+import { checkForName } from "./js/nameChecker"
+
+
+
+import './/styles/style.scss';
+import './/styles/Footer.scss';
+import './/styles/Form.scss';
+
+
+    export  {
+    handleSubmit,
+    checkForName,
+    displayResponse,
+}
