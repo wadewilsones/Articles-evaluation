3e12128656bbde442a84cf3c915290a2bc857749
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-04-21 16:05:21 -0700

    Delete website directory

--- website/client/js/nameChecker.js
+++
@@ -1,9 +0,0 @@
-function checkForName(formUrl)  {
-    
-    console.log("::: Running checkForUrl :::", formUrl);
-  
-    const pattern = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
-  
-    return pattern.test(formUrl);
-  }
-  checkForName();
